2561c655bfb77caeee910ebbcc22fc6cce85d638
Daniel <danstraw86@gmail.com>
2018-03-14 02:06:30 -0700

    completed logic to determine if input is valid. Added push to firebase of input values. Create child_added function. Uses moment.js to determine current time, difference between current time and first arrival time, and to determine next arrival time. Appends results to table. Added audio element that plays when train data entered.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,39 +11,64 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var frequency = 0;
 
+database.ref().on("child_added", function(snapshot) {
+    var row = $("<tr>");
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
+    var firstTrainConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+    var tRemainder = diffTime % frequency;
+    var minutesAway = frequency - tRemainder;
+    var nextArrival = moment().add(minutesAway, "minutes");
+    row.append("<td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + moment(nextArrival).format("hh:mm a") + "</td><td>" + minutesAway + "</td>");
+    $("tbody").append(row);
+});
+
 $("#add-train").on("click", function(event) {
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute('src', 'assets/audio/train_whistle.mp3');
     event.preventDefault();
     //user inputs data into the 4 fields.
     var name = $("#name-input").val().trim();
     var destination = $("#destination-input").val().trim();
     var firstTrain = ($("#first-train").val().trim());
-    var firstTrainArray = firstTrain.split(":")
-    var firstTrainHours = Number(firstTrainArray[0]);
-    var firstTrainMinutes = Number(firstTrainArray[1]);
-    frequency = Number($("#frequency-input").val().trim());
-    console.log(firstTrainHours);
+    var firstTrainArray = firstTrain.split(":");
+    var firstTrainHour = firstTrainArray[0];
+    var firstTrainMinute = firstTrainArray[1];
+    console.log(firstTrain[2]);
+    frequency = $("#frequency-input").val().trim();
+    console.log(isNaN(frequency));
+    
     //all fields must be filled in for data to be accepted
-    if ((name === " ") || (destination === " ") || (firstTrain === " ") || (frequency === " ")){
-        alert("all fields must be completed");
-        resetForm();
+    if ((name == "") || (destination == "") || (firstTrain == "") || (frequency == "")) {
+        $("#error").text("all fields must be completed");
         return false;
-    //first train time and frequency must be number
-    } else if ((typeof(firstTrainHours) !== "number") || (typeof(firstTrainMinutes) !== "number") || (typeof(frequency) !== "number")){
-        alert("number required");
-        resetForm();
+    } else if ((firstTrain.length !== 5) || (firstTrainHour < 0) ||(firstTrainHour >= 24) || (firstTrainMinute < 0) || (firstTrainMinute >= 60) || (firstTrain[2] !== ":")){
+        $("#error").text("Please enter first train time in military time (including the colon)");
         return false;
-    //first train: HH must be between 00 & 23, mm must be between 00 and 59
-    } else if ((firstTrainHours === undefined) || (firstTrainMinutes === undefined) || (firstTrainHours < 0) || (firstTrainHours > 23) ||(firstTrainMinutes < 0) || (firstTrainMinutes >= 60)) {
-        alert("Please enter in military time (including the colon)")
-        console.log(firstTrainHours, firstTrainMinutes);
-        resetForm();
+    } else if (isNaN(frequency)) {
+        $("#error").text("Frequency must be a number");
         return false;
-    } 
+    } else {
+        audioElement.play();
+        database.ref().push( {
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+        });
+        $("#error").remove();
+        resetForm();
+    }
 });
-
 function resetForm() {
     $("#name-input").val(this.placeholder);
     $("#destination-input").val(this.placeholder)
     $("#first-train").val(this.placeholder)
     $("#frequency-input").val(this.placeholder)
 }
+
+
